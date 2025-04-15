f9ed3f79b7946ecb10ceb5d963eb02de2d92c36c
PaulImmanuel <paulimmanuel@karunya.edu.in>
2025-04-15 20:42:09 +0530

    Update Products.js

--- Products.js
+++ Products.js
@@ -1,14 +1,5 @@
 const mongoose = require('mongoose')
 
-// const productSchema = new mongoose.Schema({
-//     name: String,
-//     quantity: Number,
-//     price: Number
-// })
-
-// const ProductModel = mongoose.model('Products', productSchema, 'Products')
-// module.exports = ProductModel
-
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -29,3 +20,9 @@ const productSchema = new mongoose.Schema({
 }, {
     timestamps: true // Adds createdAt and updatedAt fields automatically
 })
+
+// Create the Product model based on the schema
+const ProductModel = mongoose.model('Products', productSchema, 'Products')
+
+// Export the model so it can be used in other files (like server.js)
+module.exports = ProductModel
